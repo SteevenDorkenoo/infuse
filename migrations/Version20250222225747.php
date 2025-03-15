6ff668f2ec73b4e0a02ee4976943c193e25e78e5
SteevenDorkenoo <113618290+SteevenDorkenoo@users.noreply.github.com>
2025-03-15 20:28:55 +0100

    edit migration Version20250222225747 rank

--- migrations/Version20250222225747.php
+++ migrations/Version20250222225747.php
@@ -20,7 +20,7 @@ final class Version20250222225747 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE step (id INT AUTO_INCREMENT NOT NULL, recipe_id_id INT DEFAULT NULL, rank SMALLINT NOT NULL, content LONGTEXT NOT NULL, INDEX IDX_43B9FE3C69574A48 (recipe_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE step (id INT AUTO_INCREMENT NOT NULL, recipe_id_id INT DEFAULT NULL, `rank` SMALLINT NOT NULL, content LONGTEXT NOT NULL, INDEX IDX_43B9FE3C69574A48 (recipe_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE step ADD CONSTRAINT FK_43B9FE3C69574A48 FOREIGN KEY (recipe_id_id) REFERENCES recipes (id)');
     }
 
